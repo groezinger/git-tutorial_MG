6534c5fe260b4469936b051a15d57806a7ce2025
groezinger <ge93pil@mytum.de>
2022-05-17 09:41:34 +0200

    updates from coding challenge 3

--- src/main.c
+++ src/main.c
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include "espl_lib.h"
-
-
-int main(int argc, char *argv[]) {
-    int input;
-    char answer;
-    int c;
-    while(answer!='n'){
-        printf("Please type in your positive number(s): \n");
-        if(scanf("%i", &input) == 0 || input < 0){
-            printf("Invalid input! Please enter psoitive numbers! \n");
-            while ((c = getchar()) != '\n' && c != EOF) { }
-        }
-        else{
-            char* output = num_to_words(input);
-            printf("Your number(s) in words from last to first number: %s \n", output);
-            printf("Do you want to try out a different number(y/n)? \n");
-            getchar();
-            scanf("%c", &answer);
-            while (answer!='y' && answer!='n'){
-                printf("Invalid answer! Please type in y or n! \n");
-                getchar();
-                scanf("%c", &answer);
-            }
-        }
-    }
-    return 0;
-=======
 /****************************************************************************
   ----------------------------------------------------------------------
   Copyright (C) Alexander Hoffman, 2019
@@ -246,5 +216,4 @@ int main(int argc, char **argv) {
   printf("Finshed\n");
 
   return -1;
->>>>>>> conflicts
 }
